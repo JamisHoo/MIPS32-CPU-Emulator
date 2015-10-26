3ee5fd30ccfb0d0063f7e67d60eafc4adf4439b9
Jamis <hoojamis@gmail.com>
2015-10-26 22:24:42 +0800

    framework of cp0

---
+++ src/cp0.h
@@ -0,0 +1,25 @@
+
+class CP0 {
+public:
+    CP0() {
+        // TODO: initialize some reg
+    }
+
+private:
+    enum { 
+        SR = 12, 
+        Cause = 13,
+        EPC = 14,
+        Count = 9,
+        Compare = 11,
+        BadVAddr = 8,
+        Context = 4,
+        EntryHi = 10,
+        EntryLo0 = 2,
+        EntryLo1 = 3,
+        Index = 0,
+        EBase = 15
+    };
+
+    uint32_t registers_[32];
+};
