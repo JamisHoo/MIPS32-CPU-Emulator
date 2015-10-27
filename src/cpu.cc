5fabd0315e8e16617b651d9c012e89282e71542e
Jamis <hoojamis@gmail.com>
2015-10-27 20:46:08 +0800

    update CPU module

---
+++ src/cpu.cc
@@ -0,0 +1,373 @@
+void CPU::exe_sll() {
+    registers_[rd()] = registers_[rt()] << shift();
+    pc_ += 4;
+}
+
+void CPU::exe_srl() {
+    registers_[rd()] = registers_[rt()] >> shift();
+    pc_ += 4;
+}
+
+void CPU::exe_sra() {
+    int32_t val = registers_[rt()];
+    val >>= shift();
+    registers_[rd()] = val;
+    pc_ += 4;
+}
+
+void CPU::exe_sllv() {
+    registers_[rd()] = registers_[rt()] >> registers_[rs()];
+    pc_ += 4;
+}
+
+void CPU::exe_srav() {
+    int32_t val = registers_[rt()];
+    val >>= registers_[rs()];
+    registers_[rd()] = val;
+    pc_ += 4;
+}
+
+void CPU::exe_jr() {
+    pc_ = registers_[rs()];
+}
+
+void CPU::exe_jalr() {
+    registers_[rd()] = pc_ + 4;
+    pc_ = registers_[rs()];
+}
+
+void CPU::exe_syscall() {
+    // TODO: syscall
+}
+
+void CPU::exe_mfhi() {
+    registers_[rd()] = hi_;
+    pc_ += 4;
+}
+
+void CPU::exe_mthi() {
+    hi_ = registers_[rs()];
+    pc_ += 4;
+}
+
+void CPU::exe_mflo() {
+    registers_[rd()] = lo_;
+    pc_ += 4;
+}
+
+void CPU::exe_mtlo() {
+    lo_ = registers_[rs()];
+    pc_ += 4;
+}
+
+void CPU::exe_mult() {
+    int64_t a = registers_[rs()];
+    int64_t b = registers_[rt()];
+    a *= b;
+    hi_ = uint32_t(a >> 32);
+    lo_ = uint32_t(a);
+    pc_ += 4;
+}
+
+void CPU::exe_addu() {
+    registers_[rd()] = registers_[rs()] + registers_[rd()];
+    pc_ += 4;
+}
+
+void CPU::exe_subu() {
+    registers_[rd()] = registers_[rs()] - registers_[rt()];
+    pc_ += 4;
+}
+
+void CPU::exe_and() {
+    registers_[rd()] = registers_[rs()] & registers_[rt()];
+    pc_ += 4;
+}
+
+void CPU::exe_or() {
+    registers_[rd()] = registers_[rs()] | registers_[rt()];
+    pc_ += 4;
+}
+
+void CPU::exe_xor() {
+    registers_[rd()] = registers_[rs()] ^ registers_[rt()];
+    pc_ += 4;
+}
+
+void CPU::exe_slt() {
+    registers_[rd()] = int32_t(registers_[rs()]) < int32_t(registers_[rt()])? 0x01, 0x00;
+    pc_ += 4;
+}
+
+void CPU::exe_sltu() {
+    registers_[rd()] = uint32_t(registers_[rs()]) < uint32_t(registers_[rt()])? 0x01, 0x00;
+    pc_ += 4;
+}
+
+void CPU::exe_bltz() {
+    if (int32_t(registers_[rs()]) < 0)
+        pc_ += branch_offset() * 4;
+    else
+        pc_ += 4;
+}
+
+void CPU::exe_bgez() {
+    if (int32_t(registers_[rs()]) >= 0)
+        pc_ += branch_offset() * 4;
+    else
+        pc_ += 4;
+}
+
+void CPU::exe_j() {
+    pc_ = pc_ & 0xf0000000 | jump_target() * 4;
+}
+
+void CPU::exe_jal() {
+    registers_[REG_RA] = pc_ + 4;
+    pc_ = pc_ & 0xf0000000 | jump_target() * 4;
+}
+
+void CPU::exe_beq() {
+    if (registers_[rs()] == registers_[rt()])
+        pc_ += branch_offset() * 4;
+    else
+        pc_ += 4;
+}
+
+void CPU::exe_bne() {
+    if (registers_[rs()] != registers_[rt()])
+        pc_ += branch_offset() * 4;
+    else
+        pc_ += 4;
+}
+
+void CPU::exe_blez() {
+    if (int32_t(registers_[rs()]) <= 0)
+        pc_ += branch_offset() * 4;
+    else
+        pc_ += 4;
+}
+
+void CPU::exe_bgtz() {
+    if (int32_t(registers_[rs()]) > 0)
+        pc_ += branch_offset() * 4;
+    else
+        pc_ += 4;
+}
+
+void CPU::exe_addiu() {
+    registers_[rt()] = registers_[rs()] + signed_immediate();
+    pc_ += 4;
+}
+
+void CPU::exe_slti() {
+    registers_[rt()] = int32_t(registers_[rs()]) < signed_immediate()? 0x01: 0x00;
+    pc_ += 4;
+}
+
+void CPU::exe_sltiu() {
+    registers_[rt()] = registers_[rs()] < unsigned_immediate()? 0x01: 0x00;
+    pc_ += 4;
+}
+
+void CPU::exe_andi() {
+    registers_[rt()] = registers_[rs()] & unsigned_immediate();
+    pc_ += 4;
+}
+
+void CPU::exe_ori() {
+    registers_[rt()] = registers_[rs()] | unsigned_immediate();
+    pc_ += 4;
+}
+
+void CPU::exe_xori() {
+    registers_[rt()] = registers_[rs()] ^ unsigned_immediate();
+    pc_ += 4;
+}
+
+void CPU::exe_lui() {
+    registers_[rt()] = uint32_t(unsigned_immediate()) << 16;
+    pc_ += 4;
+}
+
+void CPU::exe_mfc0() {
+    registers_[rt()] = cp0_.registers_[rd()];
+    pc_ += 4;
+}
+
+void CPU::exe_mtc0() {
+    cp0_.registers_[rd()] = registers_[rt()];
+    pc_ += 4;
+}
+
+void CPU::exe_tlbwi() {
+    mmu_.tlb_key_[cp0_.registers_[cp0_.Index]] = cp0_.registers_[cp0_.EntryHi];
+    mmu_.tlb_data_[0][cp0_.registers_[cp0_.Index]] = cp0_registers_[cp0_.EntryLo0];
+    mmu_.tlb_data_[1][cp0_.registers_[cp0_.Index]] = cp0_registers_[cp0_.EntryLo1];
+    pc_ += 4;
+}
+
+void CPU::exe_eret() {
+    pc_ = cp0_.registers_[cp0_.EPC];
+    cp0_.registers_[cp0_.SR] &= 0xfffffffd;
+}
+
+void CPU::exe_lb() {
+    int8_t val = mmu_.read_byte(registers_[rs()] + signed_immediate());
+    registers_[rt()] = int32_t(val); 
+    pc_ += 4;
+}
+
+void CPU::exe_lw() {
+    uint32_t val = mmu_.read_word(registers_[rs()] + signed_immediate());
+    registers_[rt()] = val;
+    pc_ += 4;
+}
+
+void CPU::exe_lbu() {
+    uint8_t val = mmu_.read_byte(registers_[rs()] + signed_immediate());
+    registers_[rt()] = val; 
+    pc_ += 4;
+}
+
+void CPU::exe_lhu() {
+    uint16_t val = mmu_.read_half_word(registers_[rs()] + signed_immediate());
+    registers_[rt()] = val;
+    pc_ += 4;
+}
+
+void CPU::exe_sb() {
+    mmu_.write_byte(registers_[rs()] + signed_immediate(), registers_[rt()] & 0xff);
+    pc_ += 4;
+}
+
+void CPU::exe_sw() {
+    mmu_.write_word(registers_[rs()] + signed_immediate(), registers_[rt()]);
+    pc_ += 4;
+}
+
+void CPU::exe_cache() {
+    pc_ += 4;
+}
+
+void CPU::instruction_decode() {
+    switch (main_opcode(instruction)) {
+        case 0b000000: 
+            switch (sub_opcode(instruction)) {
+                // sll
+                case 0b000000:
+                // srl
+                case 0b000010:
+                // sra
+                case 0b000011:
+                // sllv
+                case 0b000100:
+                // srlv
+                case 0b000110:
+                // srav
+                case 0b000111:
+                // jr
+                case 0b001000:
+                // jalr
+                case 0b001001:
+                // syscall
+                case 0b001100:
+                // mfhi
+                case 0b010000:
+                // mthi
+                case 0b010001:
+                // mflo
+                case 0b010010:
+                // mtlo
+                case 0b010011:
+                // mult
+                case 0b011000:
+                // addu
+                case 0b100001:
+                // subu
+                case 0b100011:
+                // and
+                case 0b100100:
+                // or
+                case 0b100101:
+                // xor
+                case 0b100110:
+                // nor
+                case 0b100111:
+                // slt
+                case 0b101010:
+                // sltu
+                case 0b101011:
+                // TODO: exception
+                default:
+            }
+        case 0b000001: 
+            switch (rt(instruction)) {
+                // bltz
+                case 0b00000:
+                // bgez
+                case 0b00001:
+                // TODO: exception
+                default:
+            }
+        // j
+        case 0b000010:
+        // jal
+        case 0b000011:
+        // beq
+        case 0b000100:
+        // bne
+        case 0b000101:
+        // blez
+        case 0b000110:
+        // bgtz
+        case 0b000111:
+        // addiu
+        case 0b001001:
+        // slti
+        case 0b001010:
+        // sltiu
+        case 0b001011:
+        // andi
+        case 0b001100:
+        // ori
+        case 0b001101:
+        // xori
+        case 0b001110:
+        // lui
+        case 0b001111:
+        case 0b010000:
+            switch (sub_opcode(instruction)) {
+                case 0b000000:
+                    switch (rs(instruction)) {
+                        // mfc0
+                        case 0b00000:
+                        // mtc0
+                        case 0b00100:
+                        // TODO: exception
+                        default:
+                            
+                    }
+                // tlbwi
+                case 0b000010:
+                // eret
+                case 0b011000:
+                // TODO: exception
+                default:
+            }
+        // lb
+        case 0b100000:
+        // lw
+        case 0b100011:
+        // lbu
+        case 0b100100:
+        // lhu
+        case 0b100101:
+        // sb 
+        case 0b101000:
+        // sw
+        case 0b101011:
+        // cache
+        case 0b101111:
+    }
+}
--- src/cpu.h
+++ src/cpu.h
@@ -2,34 +2,113 @@
 #include "mem.h"
 
 class CPU {
+public:
+    CPU(std::ifstream& rom, std::ifstream& flash): mmu_(rom, flash), pc_(PC_INITIAL) {
+
+
+    }
+
+    void run() {
+        while (true) 
+            next();
+    }
     
-    uint32_t IF() {
-
-
-    }
-
-    void ID() {
-
-
-    }
-
-    void EXE() {
-
-    }
-
-    void MEM() {
-
-    }
-
-    void WB() {
-
-    }
-
-
 private:
-    CP0 cp0_;
-    Memory mem_;
+    void next() {
+        register_[REG_ZERO] = 0x00;
 
-    uint32_t registers[32];
-    uint32_t PC;
+        // instruction fetch
+        instruction_ = mmu_.read_word(pc_);
+
+        // instruction decode
+        instruction_decode();
+    }
+
+    void instruction_decode();
+
+    void exe_sll(); 
+    void exe_srl(); 
+    void exe_sra(); 
+    void exe_sllv(); 
+    void exe_srav(); 
+    void exe_jr();
+    void exe_jalr();
+    void exe_syscall();
+    void exe_mfhi();
+    void exe_mthi();
+    void exe_mflo();
+    void exe_mtlo();
+    void exe_mult();
+    void exe_addu();
+    void exe_subu();
+    void exe_and();
+    void exe_or();
+    void exe_xor();
+    void exe_nor();
+    void exe_slt();
+    void exe_sltu();
+    void exe_bltz();
+    void exe_bgez();
+    void exe_j();
+    void exe_jal();
+    void exe_beq();
+    void exe_bne();
+    void exe_blez();
+    void exe_bgtz();
+    void exe_addiu();
+    void exe_slti();
+    void exe_sltiu();
+    void exe_andi();
+    void exe_ori();
+    void exe_xori();
+    void exe_lui();
+    void exe_mfc0();
+    void exe_mtc0();
+    void exe_tlbwi();
+    void exe_eret();
+    void exe_lb();
+    void exe_lw();
+    void exe_lbu();
+    void exe_lhu();
+    void exe_sb();
+    void exe_sw();
+    void exe_cache();
+
+
+    uint32_t main_opcode() const { return instruction_ >> 26; }
+    uint32_t sub_opcode() const { return instruction_ & 0x3f; }
+    uint32_t rs() const { return instruction_ >> 21 & 0x1f; }
+    uint32_t rt() const { return instruction_ >> 16 & 0x1f; }
+    uint32_t rd() const { return instruction_ >> 11 & 0x1f; }
+    uint32_t shiff() const { return instruction_ >> 6 & 0x1f; }
+    int16_t branch_offset() const { return int16_t(instruction_ & 0xffff); }
+    uint32_t jump_target() const { return instruction_ & 0x3ffffff; }
+    int16_t singed_immediate() const { return int16_t(instruction_ & 0xffff); }
+    uint16_t unsigned_immediate() const { return uint16_t(instruction_ & 0xffff); }
+
+    constexpr uint32_t PC_INITIAL = 0xBFC00000;
+
+    CP0 cp0_;
+    MMU mmu_;
+
+    enum { 
+        REG_ZERO = 0, 
+        REG_AT = 1, 
+        REG_V0 = 2, REG_V1, 
+        REG_A0 = 4, REG_A1, REG_A2, REG_A3,
+        REG_T0 = 8, REG_T1, REG_T2, REG_T3, REG_T4, REG_T5, REG_T6, REG_T7, 
+        REG_T8 = 24, REG_T9, 
+        REG_S0 = 16, REG_S1, REG_S2, REG_S3, REG_S4, REG_S5, REG_S6, REG_S7,
+        REG_K0 = 26, REG_K1, 
+        REG_GP = 28,
+        REG_SP = 29, 
+        REG_S8 = 30, REG_FP = 30,
+        REG_RA = 31
+    };
+
+    uint32_t registers_[32];
+    uint32_t pc_;
+    uint32_t instruction_;
+    uint32_t hi_, lo_;
+
 };
