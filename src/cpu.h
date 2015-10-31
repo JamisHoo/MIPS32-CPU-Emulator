42df85df9afd9784a92deef29b048e05661a0204
Jamis <hoojamis@gmail.com>
2015-10-31 21:16:57 +0800

    resolve todo

--- src/cpu.h
+++ src/cpu.h
@@ -32,7 +32,6 @@ private:
         cp0_.registers_[cp0_.Count]++;
 
         
-        // TODO: timer interupt doesn't work
         // TODO: who is responsible for clearing Count register
         // TODO: does KSU count?
         // TODO: need to check IE/EXL before enter exception handling?
